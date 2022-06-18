415948bbcff52862c67ccf68812cf6e3e339c7ed
AgusMartino <martinoagustin4@gmail.com>
2022-06-18 17:24:33 -0300

    Nav y Login front

---
+++ front/src/index.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <NAV/>
+</div>
+<div class="container">
+    <div class="col">
+        <login/>
+    </div>
+</div>
+</template>
+
+<script>
+import NAV from "./components/nav.vue"
+import login from "./components/login.vue"
+export default{
+    components:{
+        NAV,
+        login
+    }
+}
+</script>
+<style>
+.container{
+    justify-content: center;
+    align-items: center;
+    padding-right: 25%;
+    padding-left: 25%
+}
+</style>
