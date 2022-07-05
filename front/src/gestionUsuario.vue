0382552ae692d0165d1a0c7d92ea5c5016c4169b
AgusMartino <martinoagustin4@gmail.com>
2022-07-05 20:41:48 -0300

    search de lista de usuarios

--- front/src/gestionUsuario.vue
+++ front/src/gestionUsuario.vue
@@ -1,30 +1,39 @@
 <template>
-<div>
-    <NAV/>
-</div>
-<div class="container">
+  <div>
+    <NAV />
+  </div>
+  <div class="container">
     <div class="col">
-        <h3>Listado de usuarios existentes</h3>
-        
-        <button type="button" class="btn btn-secondary">Registrar un nuevo usuario</button>
+      <h3>Listado de usuarios existentes</h3>
 
-        <input class="form-control" id="myInput" type="text" placeholder="Buscar usuario">
+      <button type="button" class="btn btn-secondary">
+        Registrar un nuevo usuario
+      </button>
 
-        <table class="table">
-            <tr>
-                <th scope="col">Usuario</th>
-                <th scope="col">Email</th>
-                <th scope="col">Modificar</th>
-            </tr>
-            <tbody id="myTable">
-                <dataTable v-for="user in entradasJSON"
-                    :Usuario="user.Usuario"
-                    :Email="user.Email"
-                />
-            </tbody>
-        </table>
+      <input
+        v-model="nombre"
+        class="form-control mt-3 mb-3"
+        id="myInput"
+        type="text"
+        placeholder="Buscar usuario"
+      />
+
+      <table class="table">
+        <tr>
+          <th scope="col">Usuario</th>
+          <th scope="col">Email</th>
+          <th scope="col">Modificar</th>
+        </tr>
+        <tbody id="myTable">
+          <dataTable
+            v-for="user in userfilter"
+            :Usuario="user.Usuario"
+            :Email="user.Email"
+          />
+        </tbody>
+      </table>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
@@ -46,21 +55,32 @@ export default{
                     Usuario: "Eze",
                     Email: "EzeAmorosino@gmail.com"
                 }
-             ]
+             ],
+             nombre: null
+        }
+    },
+    computed:{
+        userfilter(){
+                if(!this.nombre)
+                  return this.entradasJSON;
+                else
+                  return this.entradasJSON.filter((data) => {
+                  return !data.Usuario.toLowerCase().indexOf((this.nombre.toLowerCase()));
+                  })
         }
     }
 }
 </script>
 
 <style>
-.container{
-    justify-content: center;
-    align-items: center;
+.container {
+  justify-content: center;
+  align-items: center;
 }
-.col{
-    justify-content: center;
-    align-items: center;
-    padding-right: 20%;
-    padding-left: 20%
+.col {
+  justify-content: center;
+  align-items: center;
+  padding-right: 20%;
+  padding-left: 20%;
 }
 </style>
